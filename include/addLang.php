286cc0ef8ac63eab5936d7ee0488969a8bdf7ea5
Bin <Bin@Aspire.home>
2017-03-06 16:14:56 +0000

    Merge origin/master
    Conflicts:
    	index.php

---
+++ include/addLang.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once("database.php");
+
+
+$id = $_POST['id'];
+
+
+
+$query = "SELECT * from language WHERE language_id = :id ";
+$statement = $db->prepare($query);
+$statement->bindValue(":id", $id);
+$statement->execute();
+$lang = $statement->fetchAll();
+$statement->closeCursor();
+
+$language_name = $lang[0][1];
+$language_price = $lang[0][2];
+
+
+$query2 = "INSERT INTO shopping_cart (language_id,language_name,language_price) VALUES (:id,:name,:price)";
+$statement2 = $db->prepare($query2);
+$statement2->bindValue(":id", $id);
+$statement2->bindValue(":name",$language_name);
+$statement2->bindValue(":price",$language_price);
+$statement2->execute();
+$statement2->closeCursor();
+
+echo "success";
+
+
+
+
--- language.php
+++ language.php
@@ -15,11 +15,31 @@ and open the template in the editor.
     <body>
         <?php
         for ($x = 0; $x < sizeof($language); $x++) {
-                echo $language[$x]['language_name'];
-                echo $language[$x]['language_price'];
-                echo '<img src="img/'. $language[$x]["language_bgpic"].'" alt=""/>';
-               
+            echo $language[$x]['language_name'];
+            echo $language[$x]['language_price'];
+            echo '<img src="img/' . $language[$x]["language_bgpic"] . '" alt=""/>';
+            echo '<form><input onclick="addLang(1)" value="Press Me" type="button" ></form>';
         }
         ?>
+
+
+        <script>
+            function addLang(id) {
+                $(document).ready(function () {
+                    $.ajax({
+                        url: "include/addLang.php",
+                        type: "post",
+                        data: {
+                            id: id
+                        }
+                        success: function(data){
+                            alert(data);
+                        }
+
+                    });
+                });
+            }
+
+        </script>
     </body>
 </html>
